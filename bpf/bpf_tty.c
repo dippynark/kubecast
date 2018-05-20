66aa27ddbe6d3c67b0a3e5df5cdcfaed6a3aaea2
Luke Addison <luke.addison@jetstack.io>
2018-05-20 14:59:40 +0100

    Fix syntax

--- bpf/bpf_tty.c
+++ bpf/bpf_tty.c
@@ -56,10 +56,10 @@ int kprobe__tty_write(struct pt_regs *ctx)
 
     // retrieve mount namespace inum
     task = (struct task_struct *)bpf_get_current_task();
-    bpf_probe_read(&nsproxy, sizeof(nsproxy), (void *)&task->nsproxy)
-    bpf_probe_read(&mnt_ns, sizeof(mnt_ns), (void *)&nsproxy->mnt_ns)
-    bpf_probe_read(&ns_common, sizeof(ns_common), (void *)&mnt_mns->ns_common)
-    tty_write.mnt_ns_inum = ns_common.inum
+    bpf_probe_read(&nsproxy, sizeof(nsproxy), (void *)&task->nsproxy);
+    bpf_probe_read(&mnt_ns, sizeof(mnt_ns), (void *)&nsproxy->mnt_ns);
+    bpf_probe_read(&ns, sizeof(ns), (void *)&mnt_mns->ns);
+    tty_write.mnt_ns_inum = ns_common.inum;
 
     // bpf_probe_read() can only use a fixed size, so truncate to count in user space:
     bpf_probe_read(&tty_write.buf, BUFSIZE, (void *)ctx->si);
