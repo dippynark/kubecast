2a9e7b310374da63991e82c226bf07d720f6da3c
Luke Addison <luke.addison@jetstack.io>
2018-05-20 17:27:06 +0100

    Define err

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -28,7 +28,7 @@ func main() {
 	channel := make(chan []byte)
 	lostChannel := make(chan uint64)
 
-	err = kubepf.New(channel, lostChannel)
+	err := kubepf.New(channel, lostChannel)
 	if err != nil {
 		glog.Fatalf("failed to load BPF module: %s", err)
 	}
@@ -37,7 +37,7 @@ func main() {
 	for {
 
 		// connect to server
-		ws, err := websocket.Dial(fmt.Sprintf("ws://%s:%d/upload", serverAddress, port), "", fmt.Sprintf("http://%s/", serverAddress))
+		ws, err = websocket.Dial(fmt.Sprintf("ws://%s:%d/upload", serverAddress, port), "", fmt.Sprintf("http://%s/", serverAddress))
 		if err != nil {
 			glog.Errorf("failed to connect to server: %s", err)
 			sleep(1)
